d589de073268bdee6d38985edeea32a673d40fc6
Murad Kamali <murad.coding@gmail.com>
2023-09-30 00:27:52 +0100

    Updates form fields, adds assets for l ogo, refactors signupform into seperate component from the tutor hero

--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,4 +1,3 @@
-import { Navbar } from "@/components/Navbar";
 import SignUpForm from "@/components/SignUpForm";
 
 export default function Home() {
@@ -10,7 +9,18 @@ export default function Home() {
         illo, possimus, similique impedit modi odio quia optio consequatur minus
         alias aliquid neque quasi, rem odit ab quo harum consectetur nemo.
       </p>
-      <SignUpForm />
+      <div className="HeroContainer md:flex space-x-4 text-black ">
+        <div className="flex-grow md:max-w-[500px]  HeroHeader mt-20 text-white">
+          <h2 className="text-center text-bold text-2xl md:text-6xl">
+            Become A Tutor!
+          </h2>
+          <h3 className="text-center text-sm md:text-2xl italic mt-12">
+            Fill in the details to join our community of knowledge sharers and
+            become a tutor!
+          </h3>
+        </div>
+        <SignUpForm />
+      </div>
     </main>
   );
 }
--- client/src/components/SignUpForm.tsx
+++ client/src/components/SignUpForm.tsx
@@ -1,107 +1,148 @@
-import React from "react";
+"use client";
 
+import React from "react";
+import { useForm, SubmitHandler } from "react-hook-form";
 type Props = {};
 
+type FormInput = {
+  name: string;
+  phoneNumber: string;
+  email: string;
+  subject: string;
+  address: string;
+  city: string;
+  postCode: string;
+};
+
 const SignUpForm = (props: Props) => {
+  const { register, handleSubmit } = useForm<FormInput>();
+
+  const onSubmit: SubmitHandler<FormInput> = async (data) => {
+    try {
+      const res = await fetch(`${process.env.NEXT_PUBLIC_API_URL}/tutor`, {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(data),
+      });
+
+      const responseData = await res.json();
+      console.log("Success:", responseData);
+    } catch (error) {
+      console.log("Error: " + error);
+    }
+
+    console.log(data); // This is where you can send it to your API
+  };
+
   return (
-    <div className="SignUpContainer md:flex space-x-4 text-black ">
-      <div className="flex-grow md:max-w-[500px]  SignUpHeader mt-20 text-white">
-        <h2 className="text-center text-bold text-2xl md:text-6xl">
-          Become A Tutor!
-        </h2>
-        <h3 className="text-center text-sm md:text-2xl italic mt-12">
-          Fill in the details to join our community of knowledge sharers and
-          become a tutor!
-        </h3>
+    <form
+      className="flex-grow bg-white  dark:text-white dark:bg-gray-800 space-y-4 md:space-y-6 mt-10 rounded-2xl p-6 md:px-20 md:py-10"
+      action="#"
+      onSubmit={handleSubmit(onSubmit)}
+    >
+      <div>
+        <label htmlFor="name" className="block mb-2 text-sm font-medium">
+          Name
+        </label>
+        <input
+          type="text"
+          id="name"
+          className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-red-600 focus:border-red-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="John Smith"
+          {...register("name", { required: true })}
+        />
       </div>
-      <form
-        className="flex-grow bg-white  dark:text-white dark:bg-gray-800 space-y-4 md:space-y-6 mt-10 rounded-2xl p-6 md:px-20 md:py-10"
-        action="#"
+      <div>
+        <label htmlFor="phoneNumber" className="block mb-2 text-sm font-medium">
+          Phone Number
+        </label>
+        <input
+          type="number"
+          id="phoneNumber"
+          className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="07123 456789"
+          style={{ appearance: "textfield" }} // Removes the increment/decrement buttons
+          {...register("phoneNumber", { required: true })}
+        />
+      </div>
+      <div>
+        <label htmlFor="email" className="block mb-2 text-sm font-medium">
+          Your email
+        </label>
+        <input
+          type="email"
+          id="email"
+          className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="name@company.com"
+          {...register("email", { required: true })}
+        />
+      </div>
+      <div>
+        <label htmlFor="subject" className="block mb-2 text-sm font-medium">
+          Subjects
+        </label>
+        <input
+          type="string"
+          id="subject"
+          className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="Maths, Physics, Computer Science..."
+          {...register("subject", { required: true })}
+        />
+      </div>
+      <div>
+        <label htmlFor="address" className="block mb-2 text-sm font-medium">
+          Address
+        </label>
+        <input
+          type="text"
+          id="address"
+          className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="123 Elm Street"
+          {...register("address", { required: true })}
+        />
+      </div>
+      <div>
+        <label htmlFor="city" className="block mb-2 text-sm font-medium">
+          City
+        </label>
+        <input
+          type="text"
+          id="city"
+          className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="London"
+          {...register("city", { required: true })}
+        />
+      </div>
+      <div>
+        <label htmlFor="postCode" className="block mb-2 text-sm font-medium">
+          Post Code
+        </label>
+        <input
+          type="text"
+          id="postCode"
+          className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
+          placeholder="SW1A 1AA"
+          {...register("postCode", { required: true })}
+        />
+      </div>
+      <button
+        type="submit"
+        className="w-full text-white bg-[#197278] hover:bg-green-00 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800"
       >
-        <div>
-          <label htmlFor="name" className="block mb-2 text-sm font-medium">
-            Name
-          </label>
-          <input
-            type="text"
-            name="name"
-            id="name"
-            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-red-600 focus:border-red-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="John Smith"
-            required={true}
-          />
-        </div>
-        <div>
-          <label
-            htmlFor="phoneNumber"
-            className="block mb-2 text-sm font-medium"
-          >
-            Phone Number
-          </label>
-          <input
-            type="number"
-            name="phoneNumber"
-            id="phoneNumber"
-            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="07123 456789"
-            required={true}
-            style={{ appearance: "textfield" }} // Removes the increment/decrement buttons
-          />
-        </div>
-        <div>
-          <label htmlFor="email" className="block mb-2 text-sm font-medium">
-            Your email
-          </label>
-          <input
-            type="email"
-            name="email"
-            id="email"
-            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="name@company.com"
-            required={true}
-          />
-        </div>
-        <div>
-          <label htmlFor="address" className="block mb-2 text-sm font-medium">
-            Address
-          </label>
-          <input
-            type="text"
-            name="address"
-            id="address"
-            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="123 Elm Street"
-            required={true}
-          />
-        </div>
-        <div>
-          <label htmlFor="city" className="block mb-2 text-sm font-medium">
-            City
-          </label>
-          <input
-            type="text"
-            name="city"
-            id="city"
-            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="London"
-            required={true}
-          />
-        </div>
-        <div>
-          <label htmlFor="postCode" className="block mb-2 text-sm font-medium">
-            Post Code
-          </label>
-          <input
-            type="text"
-            name="postCode"
-            id="postCode"
-            className="bg-gray-50 border border-gray-300 text-gray-900 sm:text-sm rounded-lg focus:ring-primary-600 focus:border-primary-600 block w-full p-2.5 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
-            placeholder="SW1A 1AA"
-            required={true}
-          />
-        </div>
-      </form>
-    </div>
+        Create an account
+      </button>
+      <p className="text-sm font-light text-gray-500 dark:text-gray-400">
+        Already have an account?{" "}
+        <a
+          href="#"
+          className="font-medium text-primary-600 hover:underline dark:text-primary-500"
+        >
+          Login here
+        </a>
+      </p>
+    </form>
   );
 };
 
